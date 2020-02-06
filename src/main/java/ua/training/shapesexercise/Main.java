ae1a7294d3d51546353cb0c37b5478f3eccec1e6
Olesiiuk Anna <olesiiuk.anna@gmail.com>
2020-02-06 17:04:19 +0200

    Added package model and new class Paralellogram

--- src/main/java/ua/training/shapesexercise/Main.java
+++ src/main/java/ua/training/shapesexercise/Main.java
@@ -1,10 +1,17 @@
 package ua.training.shapesexercise;
 
+import ua.training.shapesexercise.model.Parallelogram;
+import ua.training.shapesexercise.model.Rectangle;
+
 public class Main {
 
     public static void main(String[] args) {
-        Rectangle rectangle=new Rectangle(5,8);
-rectangle.rectanglePrint();
+        Rectangle rectangle = new Rectangle(5, 8);
+        rectangle.rectanglePrint();
+
+        Parallelogram parallelogram = new Parallelogram(4, 8);
+        parallelogram.printParallelogram();
+
     }
 
 }
