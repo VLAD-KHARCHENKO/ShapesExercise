a7b76e3861b50ba89ba136d2b97f02b9273c3cb3
VLAD-KHARCHENKO <vladuslav.kharchenko.jd@gmail.com>
2020-02-06 16:34:03 +0200

    Rectangle

--- src/main/java/ua/training/shapesexercise/Main.java
+++ src/main/java/ua/training/shapesexercise/Main.java
@@ -3,7 +3,8 @@ package ua.training.shapesexercise;
 public class Main {
 
     public static void main(String[] args) {
-
+        Rectangle rectangle=new Rectangle(5,8);
+rectangle.rectanglePrint();
     }
 
 }
